5fc1fe03195d82671e27ab16b009c75a2bea634c
Romaisaa <romaisaa_elsaidy@stud.cu.edu.eg>
2022-11-04 17:48:22 +0200

    edit frequency

--- Functions.py
+++ Functions.py
@@ -1,6 +1,5 @@
 import plotly.graph_objects as go
-import streamlit as st  # 🎈 data web app development
-import pandas as pd  # read csv, df manipulation
+import streamlit as st 
 import numpy as np
 import plotly as pt
 import wave
@@ -13,23 +12,20 @@ def readAudioFile(fileName):
     return audio_file, audio_player
 
 
-def plotTimeDomain(data, time):
-
+def plotTimeDomain(time, data ):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=time, y=data,
-                             mode='lines', name='Signal Plot'))
-
+                            mode='lines', name='Signal Plot'))
     fig.update_layout(title='Time Domain')
     fig.update_xaxes(title='Time')
     fig.update_yaxes(title='Signal Value')
     st.plotly_chart(fig)
 
 
-def plotFrequencyDomain(data, time):
+def plotFrequencyDomain(frequency,frequency_magnitude):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=time, y=data,
-                             mode='lines', name='Signal Plot'))
-
+    fig.add_trace(go.Scatter(x=frequency, y=frequency_magnitude,
+                            mode='lines', name='Signal Plot'))
     fig.update_layout(title='Frequency Domain')
     fig.update_xaxes(title='Frequency (Hz)')
     fig.update_yaxes(title='Magnitude')
@@ -37,7 +33,17 @@ def plotFrequencyDomain(data, time):
 
 
 def frequencyDomain(signal, sample_rate):
-    fft_specturm = np.abs(np.fft.rfft(signal))
-    freq = np.fft.rfftfreq(signal.size, 1/sample_rate)
+    freq_magnitude = np.fft.rfft(signal)
+    freq_magnitude_abs = np.abs(freq_magnitude)
+    fft_spectrum = np.fft.rfftfreq(signal.size, 1/sample_rate)
+    return  freq_magnitude_abs,fft_spectrum
 
-    return fft_specturm, freq
+
+
+## list = [{frequency_1: 5, frequency_2: 10 new_amplitude}]
+
+def edit_frequency(freq_spectrum,frequency_magnitude, frequency_1, frequency_2,new_amplitude):
+    for i,f in enumerate(frequency_magnitude):
+        if f > frequency_1 and f < frequency_2 :
+            freq_spectrum[i]= new_amplitude
+    return freq_spectrum
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 import streamlit_vertical_slider as svs
 import Functions as fn
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-st.set_page_config(page_title="Equalizer",
-                   page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Equalizer",page_icon=":bar_chart:", layout="wide")
 
 col1, col2, col3, col4, col5, col6, col7, col8, col9, col10 = st.columns(10)
 
@@ -17,25 +17,23 @@ with col1:
                                   track_color='lightgray',  # optional
                                   thumb_color='blue'  # optional
                                   )
-
 uploadedAudio = st.file_uploader('Upload your audio here!')
+
 
 if (uploadedAudio):
     audio_file, audio_player = fn.readAudioFile(uploadedAudio.name)
     sample_freq = audio_file.getframerate()
     n_samples = audio_file.getnframes()
     single_wave = audio_file.readframes(-1)
-
     t_audio = n_samples / sample_freq
-
     signal_array = fn.np.frombuffer(single_wave, dtype=fn.np.int16)
     time = fn.np.linspace(0, t_audio, n_samples)
-
-    fn.plotTimeDomain(signal_array, time)
-
-    signal_fd, time_fd = fn.frequencyDomain(
-        signal_array, sample_freq)
-
-    fn.plotFrequencyDomain(signal_fd, time_fd)
-
+    fn.plotTimeDomain(time,signal_array)
+    freq_magnitude,frequency_spectrum_abs = fn.frequencyDomain(signal_array, sample_freq)
+    fn.plotFrequencyDomain(frequency_spectrum_abs, freq_magnitude)
     st.audio(audio_player, format='audio/wav')
+    # frequency_spectrum_abs= fn.edit_frequency(freq_magnitude,frequency_spectrum_abs,50,100,500)
+
+
+
+

9d29f578f18871a770aadd92f1d94d480c30ea42
Romaisaa <romaisaa_elsaidy@stud.cu.edu.eg>
2022-11-05 13:50:25 +0200

    inverse Fourier & signal_wav function

--- Functions.py
+++ Functions.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly as pt
 import wave
 from scipy.io.wavfile import read, write
+from scipy.io import wavfile
 
 
 def readAudioFile(fileName):
@@ -35,8 +36,9 @@ def plotFrequencyDomain(frequency,frequency_magnitude):
 def frequencyDomain(signal, sample_rate):
     freq = np.fft.rfft(signal)
     freq_magnitude= np.abs(freq)
+    freq_phase=np.angle(freq,deg=False)
     fft_spectrum = np.fft.rfftfreq(signal.size, 1/sample_rate)
-    return  freq_magnitude,fft_spectrum
+    return  freq_magnitude,freq_phase,fft_spectrum
 
 
 ## list = [{frequency_1: 5, frequency_2: 10, gain_db:2}]
@@ -45,3 +47,16 @@ def edit_frequency(freq_spectrum,freq_magnitude,sample_rate, edit_list):
     for edit in edit_list:
         freq_magnitude[int(frequency_points*edit["frequency_1"]):int((frequency_points*edit["frequency_2"]))]= np.sqrt((10**(edit['gain_db']/10)*(freq_magnitude[int(frequency_points*edit["frequency_1"]):int((frequency_points*edit["frequency_2"]))]**2)))
     return freq_magnitude
+
+
+def inverse_fourier(mag, phase):
+    complex_rect = mag * np.cos((phase))+ 1j*mag * np.sin((phase))
+    inverse_forurier= np.fft.irfft(complex_rect)
+    return inverse_forurier
+
+
+
+def signal_to_wav(signal,sample_rate):
+    signal=np.int16(signal)
+    wavfile.write("file.wav",2*sample_rate,signal)
+    return 
